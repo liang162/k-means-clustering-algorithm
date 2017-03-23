f51e0cbbe9236024ce46ca7e60d5f0902c331951
Yinrong "David" Liang <liang162@purdue.edu>
2017-03-23 17:12:31 -0400

    Initial commit

---
+++ kmeans.py
@@ -0,0 +1,11 @@
+import sys
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+#setting up, reading the parameter
+dataFilename = sys.argv[1]
+K = sys.argv[2]
+clusteringOption = sys.argv[3]
+plotOption = sys.argv[4]
+
