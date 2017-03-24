82223b5c37ff8a2e42527f11fff25ae646fc390b
Yinrong "David" Liang <liang162@purdue.edu>
2017-03-23 21:35:48 -0400

    random list

--- kmeans.py
+++ kmeans.py
@@ -25,16 +25,24 @@ temp = []
 #the array that filtered out useless information
 result = []
 
+#a list of k random numbers
+random_list = []
+
 #open csv file
 with open(dataFilename) as csvfile:
     temp_data = csv.reader(csvfile, delimiter=',')
     for row in temp_data:
         data.append(row)
 
+#take transpose
 for i in range(0, 19):
     if(i == 3 or i == 4 or i == 6 or i == 7):
         for j in range(1, len(data)):
             temp.append(data[j][i])
         result.append(temp)
         temp = []
+
+#generate a random list
+for i in range(0, int (numOfClusters)):
+    random_list.append(int(random.random() * 21090))
 
